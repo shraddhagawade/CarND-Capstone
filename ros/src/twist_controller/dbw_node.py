c1a86aa795d333d26d8af6f6c565b9fc51ec5db4
Navin Rahim <36614932+navinrahim@users.noreply.github.com>
2018-06-02 04:00:35 -0700

    Fixed Subscriber msg type

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -68,7 +68,7 @@ class DBWNode(object):
         # TODO: Subscribe to all the topics you need to
         rospy.Subscriber('/current_velocity', TwistStamped, self.velocity_cb)
         rospy.Subscriber('/twist_cmd', TwistStamped, self.twist_cb)
-        rospy.Subscriber('/vehicle/dbw_enabled', SteeringCmd, self.dbw_enabled_cb)
+        rospy.Subscriber('/vehicle/dbw_enabled', Bool, self.dbw_enabled_cb)
         
         self.current_velocity = None
         self.linear_velocity = None
