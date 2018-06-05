613f8f7c70151dc7185f09c730e4d8d166af136a
root <navin.rahim@gmail.com>
2018-06-05 10:35:43 +0000

    Traffic light testing

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -3,7 +3,18 @@ from styx_msgs.msg import TrafficLight
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
-        pass
+        tf.reset_default_graph()
+        self.sess = tf.Session()
+        
+        saver = tf.train.import_meta_graph('model.ckpt.meta')
+        saver.restore(sess,tf.train.latest_checkpoint('./'))
+            
+        rospy.loginfo('Model Restored')
+        
+        self.graph = tf.get_default_graph()
+        self.prediction = graph.get_tensor_by_name("prediction")
+        self.input = graph.get_tensor_by_name("input")        
+        self.keep_prob = graph.get_tensor_by_name("keep_prob")
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -16,4 +27,7 @@ class TLClassifier(object):
 
         """
         #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        pred = self.sess.run(self.prediction, feed_dict={self.input:image, self.keep_prob:1.}
+        labels = [TrafficLight.GREEN,TrafficLight.UNKNOWN,TrafficLight.RED,TrafficLight.YELLOW]
+        
+        return labels[pred]
