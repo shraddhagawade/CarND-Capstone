368ee26dc9239316d2436c6cf88b9d5bb6624fee
Navin Rahim <36614932+navinrahim@users.noreply.github.com>
2018-06-06 16:17:29 +0530

    Update tl_classifier.py

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -7,25 +7,7 @@ import numpy as np
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
-        tf.reset_default_graph()
-        
-        frozen_graph="./light_classification/model.pb"
-        with tf.gfile.GFile(frozen_graph, "rb") as f:
-            restored_graph_def = tf.GraphDef()
-            restored_graph_def.ParseFromString(f.read())
-        with tf.Graph().as_default() as graph:
-            tf.import_graph_def(
-            restored_graph_def,
-            input_map=None,
-            return_elements=None,
-            name=""
-        )
-        self.graph = graph
-        self.sess = tf.Session(graph=graph)
-        
-        self.prediction = self.graph.get_tensor_by_name("prediction:0")
-        self.input = self.graph.get_tensor_by_name("input:0")        
-        self.keep_prob = self.graph.get_tensor_by_name("keep_prob:0")
+        pass
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
